d8a58e1214db3891b0f0a56aeaf58625ad512079
DenisIsmagilov1 <denchikism789@gmail.com>
2020-07-08 12:35:43 +0300

    Пофисксил наименование колонок в таблице счетов

--- src/components/BillsList.vue
+++ src/components/BillsList.vue
@@ -1,11 +1,22 @@
 <template>
   <b-col cols="8" class="bills__table">
-    <b-table striped hover :items="getBills"></b-table>
+    <b-table striped hover :items="getBills" :fields="fields">
+      <template v-slot:cell(index)="data">{{ data.index + 1 }}</template>
+    </b-table>
   </b-col>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      fields: [
+        "index",
+        { key: "title", label: "Название" },
+        { key: "balance", label: "Сумма" }
+      ]
+    };
+  },
   computed: {
     getBills() {
       return this.$store.getters.bills;
